75a349a86188f20456d92179ffaf712fe8ef74b9
swanapole <swanapoleshilla@yahoo.com>
2018-12-20 13:13:47 +0300

    add all changes made

---
+++ Router/js/scripts.js
@@ -0,0 +1,17 @@
+
+var modal = document.getElementById('id01');
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
